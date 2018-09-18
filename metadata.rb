f43c8f2ab6554a8a3ac8e11b4fbbefc853926bf4
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-09-19 02:03:57 +0400

    Fix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Install libmaxminddb'
 long_description ::IO.read(::File.join(::File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 
 provides 'libmaxminddb::default'
 recipe 'libmaxminddb::default', 'Install or upgrade libmaxminddb'
